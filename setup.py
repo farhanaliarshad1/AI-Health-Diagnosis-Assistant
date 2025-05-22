b30cf84bbbe39e54ef13c20678066565416b2f7d
Farhan Ali Arshad <s2faarsh@uni-trier.de>
2025-05-22 19:28:44 +0200

    Folder structure setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Farhan Ali Arshad',
+    author_email= 'farhanali.arshad217@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
